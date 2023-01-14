8022d16860fbde61a0a9d8a9c103d81aa1855a6f
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-14 02:13:47 -0300

    Delete aula 03 directory

--- aula 03/Aula-03/src/main/kotlin/Main.kt
+++
@@ -1,7 +0,0 @@
-fun main(args: Array<String>) {
-   var nome = "gabriel"
-   var idade = 19
-   var profissão = "programador android"
-   println("Meu nome é "+nome+"  eu tenho "+idade+" anos e sou "+profissão)
-   println("meu nome é $nome eu tenho $idade anos e sou $profissão")
-}

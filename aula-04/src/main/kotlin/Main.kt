dbde130202b12ade045d8f72d041d5c4e276276a
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-14 18:19:58 -0300

    Add files via upload

---
+++ aula-04/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>) {
+ /*
+   + soma
+    - subtração
+    / divisão
+    * multiplicação
+    % modulo - resto da divisão
+    -- decremento
+    ++ incremento
+  */
+   var numero1 = 20
+    var numero2 = 10
+
+    println(numero1 + numero2)
+    println (numero1 - numero2)
+    println (numero1 / numero2)
+    println (numero1 * numero2)
+    println (numero1 % numero2)
+    println (++ numero1  )
+    println (-- numero2  )
+}

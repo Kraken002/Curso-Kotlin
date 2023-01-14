733fc17fc108bfd92164a7a1a3813bd58d208632
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-14 18:28:38 -0300

    Add files via upload

---
+++ aula-02/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+//String - textos
+//byte - números inteiros (-128 a 127)
+//int - números inteiros (-2147483648 até 2147483647)
+//long - números inteiros ( -9.223.372.036.854.775.807 até 9,223,372,036,854,775,807)
+//float - números decimais (1.123456789 até 7 casas decimais)
+//double - números decimais (1.123456789 até 15 casas decimais)
+//    boolean - valores booleanos (Estados) - true ou false
+
+//automaticamente o kotlin pode definir a tipagem dos valores
+    val nomeCompleto = "gabriel de oliveira lopes"
+    val idade = 28
+    println(nomeCompleto)
+    println(idade)
+}

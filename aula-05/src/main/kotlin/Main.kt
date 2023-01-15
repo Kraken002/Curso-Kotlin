f622367a89ec759fd1a30a0d84e49fa11f1e9674
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-15 20:22:52 -0300

    Add files via upload

---
+++ aula-05/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    /*
+    = igual
+    < menor
+    > maior
+    <=menor ou igual
+    >=maior ou igual
+    =! diferente
+     */
+
+    var altura = 8
+    var peso = 10
+    println(peso>altura)
+}

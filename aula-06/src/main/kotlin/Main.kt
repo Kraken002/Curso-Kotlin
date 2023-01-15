892d46b1773f2e2d20ee74ec8441c342d37226d9
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-15 20:23:09 -0300

    Add files via upload

---
+++ aula-06/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+var idade=14
+
+
+    if(idade <= 12){
+println("criança")
+    }else if(idade <=17){
+        println("adolescente")
+    }else {
+        println("adulto")
+    }
+
+
+}

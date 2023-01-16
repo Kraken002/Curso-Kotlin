982591bd202247cfbb44c66b6468b792936e95ed
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-16 15:41:36 -0300

    Add files via upload

---
+++ aula-07/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+//&& e
+// || ou
+
+    if(20 == 20 && 10 <20){
+        println("verdadeiro")
+    }else{
+        println("falso")
+    }
+    if(20 == 20 || 30 <20){
+        println("verdadeiro")
+    }else{
+        println("falso")
+    }
+
+}

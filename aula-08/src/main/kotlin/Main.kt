e1e4c7c35336a4273ff9f7577f1f4adcf97694a7
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-16 16:19:09 -0300

    Add files via upload

---
+++ aula-08/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+import java.util.Arrays
+
+fun main(args: Array<String>) {
+
+    var listaCompras = arrayOf("arroz","ovos","frango","aveia","banana","leite")
+
+   println(listaCompras[2])
+    println(Arrays.toString(listaCompras))
+    println(listaCompras.size)
+}

81810a8674749fa69f043b369e589990d291bcc1
Gabriel Lopes <103292390+Kraken002@users.noreply.github.com>
2023-01-17 15:41:32 -0300

    Add files via upload

---
+++ aula-09/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    var listaCompras = arrayOf("carne","feijão","arroz","ovos","frango","aveia","banana","leite")
+
+   var indice = 0
+
+   while (indice <= 8){
+       println(listaCompras[indice])
+       indice++
+   }
+}
